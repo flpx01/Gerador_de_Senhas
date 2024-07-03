50e10324d7741656ab6b02f0f90e308373b911be
Felipe Moreira <felipepenha01@gmail.com>
2024-07-03 12:02:00 -0400

    implementando o arquivo main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import { nanoid } from 'nanoid';
+
+const passwordBts = document.querySelector('button')
+const displayPasswordEl = document.querySelector('h2')
+
+passwordBts.addEventListener('click', () =>{
+    const randomPassword = nanoid()
+    displayPasswordEl.innerHTML = randomPassword
+})
