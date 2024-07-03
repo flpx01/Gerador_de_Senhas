aa843c86d154e9bcf7703b43d8c637cb51d82c78
Felipe Moreira <felipepenha01@gmail.com>
2024-07-03 19:58:00 -0400

    finalizador do gerador de senhas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { nanoid } from 'nanoid';
+import Copy from 'clipboard-copy';
+import './style.css'
 
 const passwordBts = document.querySelector('button')
 const displayPasswordEl = document.querySelector('h2')
@@ -7,3 +9,7 @@ passwordBts.addEventListener('click', () =>{
     const randomPassword = nanoid()
     displayPasswordEl.innerHTML = randomPassword
 })
+displayPasswordEl.addEventListener('click', (event) => {
+    Copy(event.target.innerHTML);
+    alert('Senha copiada!');
+  });
